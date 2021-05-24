9af97819be2d68177dadc96c9303bd865c9f9b64
Yuan-Chi Chang <yuanchi@us.ibm.com>
2021-05-24 09:07:57 -0500

    placeholder to test Or node

---
+++ codeflare/tests/test_or.py
@@ -0,0 +1,54 @@
+import pytest
+import ray
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from hercules.Datamodel import Xy
+from hercules.Datamodel import XYRef
+import hercules.Datamodel as dm
+import hercules.RuntimeNew as rt
+from hercules.RuntimeNew import ExecutionType
+
+def test_or():
+
+    ray.init()
+    
+    train = pd.read_csv('../resources/data/train_ctrUa4K.csv')
+    test = pd.read_csv('../resources/data/test_lAUu6dG.csv')
+
+    X = train.drop('Loan_Status', axis=1)
+    y = train['Loan_Status']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    X_ref = ray.put(X_train)
+    y_ref = ray.put(y_train)
+
+    Xy_ref = XYRef(X_ref, y_ref)
+    Xy_ref_list = [Xy_ref]
+
+    pipeline = dm.Pipeline()
+    node_a = dm.OrNode('preprocess', preprocessor)
+    node_b = dm.OrNode('c_a', c_a)
+    node_c = dm.OrNode('c_b', c_b)
+    
+    pipeline.add_edge(node_a, node_b)
+    pipeline.add_edge(node_a, node_c)
+
+    in_args={node_a: Xy_ref_list}
+    out_args = rt.execute_pipeline(pipeline, ExecutionType.FIT, in_args)
+
+    node_b_out_args = ray.get(out_args[node_b])
+    node_c_out_args = ray.get(out_args[node_c])
+
+    b_out_xyref = node_b_out_args[0]
+
+    ray.get(b_out_xyref.get_Xref())
+
+
+if __name__ == "__main__":
+    sys.exit(pytest.main(["-v", __file__]))
+
+
