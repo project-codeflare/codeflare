5dfda10d3606e120db015cc98aad840017f06085
CARLOS ANDRADE COSTA <chcost@us.ibm.com>
2021-06-19 21:54:39 -0400

    Updated docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,8 +26,8 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
 from recommonmark.transform import AutoStructify
 
