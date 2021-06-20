0cea14f9d7c6f6531e1b62559b2e8db7c76ef78c
CARLOS ANDRADE COSTA <chcost@us.ibm.com>
2021-06-19 23:33:22 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@
 import os
 import sys
 #sys.path.insert(0, os.path.abspath('../../codeflare/'))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
 import sphinx_rtd_theme
 from recommonmark.transform import AutoStructify
 
