82569895877d1f961dc6be783dc308e9b5b1f470
CARLOS ANDRADE COSTA <chcost@us.ibm.com>
2021-06-19 22:26:54 -0400

    Updated conf.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,6 +75,8 @@ source_suffix = {
 
 numpydoc_show_class_members = False 
 
+autosummary_generate = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
